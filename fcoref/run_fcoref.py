85c8cf7bf895bc6550137ad4438dbdf75c7de409
ShirApp <shirashury@gmail.com>
2024-06-09 17:23:53 +0300

    fcoref

--- fcoref/run_fcoref.py
+++ fcoref/run_fcoref.py
@@ -6,16 +6,21 @@ debug = True if os.path.exists("/Users/shir/PycharmProjects") else False
 
 if debug:
     dataset = load_dataset("datajuicer/the-pile-pubmed-abstracts-refined-by-data-juicer")
+    model = FCoref()
 else:
     dataset = load_dataset("qualis2006/PUBMED_title_abstracts_2020_baseline")
+    model = FCoref(device='cuda:0')
 
 abstracts_text = dataset['train']['text']
-
-model = FCoref()
 
 preds = model.predict(
     texts=abstracts_text, max_tokens_in_batch=10000
 )
-for pred in preds:
-    print(pred.get_clusters(), pred.get_clusters(as_strings=False))
 
+
+for pred in preds:
+    chains = pred.get_clusters()
+    indices = pred.get_clusters(as_strings=False)
+    for chain, index in zip(chains, indices):
+        print('\t'.join(chain))
+        print(index)
