addbd049da0488a6681cd76c9fad48c505342cbc
ShirApp <shirashury@gmail.com>
2024-06-09 22:50:11 +0300

    bet

--- betweenness/analyze_between.py
+++ betweenness/analyze_between.py
@@ -7,33 +7,35 @@ from tqdm import tqdm
 
 file_graph_clean = "data/weighted_graph_clean2.csv"
 
-df = pd.read_csv(file_graph_clean)
-pairs_dict = []
+if os.path.exists(file_graph_clean):
 
-pairs = defaultdict(tuple)
+    df = pd.read_csv(file_graph_clean)
+    pairs_dict = []
 
-for index, row in df.iterrows():
-    ph1 = row['node1']
-    ph2 = row['node2']
-    pairs[(ph1, ph2)] = {"1": 0, "2": 0, "0": 0}
-    # if len(pairs) > 10000:
-    #     break
+    pairs = defaultdict(tuple)
 
-between_path = "output/"
-for between_file in tqdm(os.listdir(between_path)):
-    try:
-        with open(between_path + between_file, "r") as f:
-            data = json.load(f)
-            for ph1, ph2 in pairs.keys():
-                if ph1 in data and ph2 in data:
-                    if data[ph1] > data[ph2]:
-                        pairs[(ph1, ph2)]["1"] += 1
-                    elif data[ph1] < data[ph2]:
-                        pairs[(ph1, ph2)]["2"] += 1
-                    else:
-                        pairs[(ph1, ph2)]["0"] += 1
-    except:
-        print("error file ", between_file)
+    for index, row in df.iterrows():
+        ph1 = row['node1']
+        ph2 = row['node2']
+        pairs[(ph1, ph2)] = {"1": 0, "2": 0, "0": 0}
+        # if len(pairs) > 10000:
+        #     break
 
-with open("analysis/result.pkl", "wb") as f:
-    pickle.dump(pairs, f)
+    between_path = "output/"
+    for between_file in tqdm(os.listdir(between_path)):
+        try:
+            with open(between_path + between_file, "r") as f:
+                data = json.load(f)
+                for ph1, ph2 in pairs.keys():
+                    if ph1 in data and ph2 in data:
+                        if data[ph1] > data[ph2]:
+                            pairs[(ph1, ph2)]["1"] += 1
+                        elif data[ph1] < data[ph2]:
+                            pairs[(ph1, ph2)]["2"] += 1
+                        else:
+                            pairs[(ph1, ph2)]["0"] += 1
+        except:
+            print("error file ", between_file)
+
+    with open("analysis/result.pkl", "wb") as f:
+        pickle.dump(pairs, f)
