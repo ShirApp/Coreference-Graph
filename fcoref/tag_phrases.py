d7e65028a32eea02a613f48fad81492e4643c94d
ShirApp <shirashury@gmail.com>
2024-06-16 00:07:25 +0300

    tag ph

--- fcoref/tag_phrases.py
+++ fcoref/tag_phrases.py
@@ -27,16 +27,39 @@ with open(nodes_dir, 'rb') as f:
         if node in dict_spell:
             temp_dict[node] = dict_spell[node]
             c_in += 1
-        else:
-            c_out += 1
-            for k in dict_spell.keys():
-                if len(node.split()) == 1:
-                    if node in k.split():
-                        print(node, "|", k)
-                        break
-                else:
-                    if node in k:
-                        print(node, "|", k)
-                        break
+        # else:
+        #     c_out += 1
+        #     for k in dict_spell.keys():
+        #         if len(node.split()) == 1:
+        #             if node in k.split():
+        #                 temp_dict[node] = temp_dict[k]
+        #                 print(node, "|", k)
+        #                 break
+        #         else:
+        #             if node in k:
+        #                 temp_dict[node] = temp_dict[k]
+        #                 print(node, "|", k)
+        #                 break
+
 
 print("in", c_in, "out", c_out)
+
+tags_dict = defaultdict()
+for k, v in temp_dict.items():
+    strings = v.keys()
+    for s in strings:
+        if s[0].islower():
+            tags_dict[k] = "noun"
+            continue
+    dominant_str = max(v, key=v.get)
+    # print(dominant_str)
+    if dominant_str.isupper() and dominant_str.isalpha():
+        tags_dict[k] = "abb"
+        continue
+    tags_dict[k] = "name"
+
+output_dir = "./output_tags"
+os.makedirs(output_dir, exist_ok=True)
+
+with open(os.path.join(output_dir, "phrases_tags" + out_file + ".pkl"), "wb") as f_spell:
+    pickle.dump(tags_dict, f_spell)
