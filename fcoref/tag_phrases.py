71d7ec3b7a049b1bb425d303f82627d368d3084e
ShirApp <shirashury@gmail.com>
2024-06-15 23:29:06 +0300

    tag ph

--- fcoref/tag_phrases.py
+++ fcoref/tag_phrases.py
@@ -5,18 +5,20 @@ debug = True if os.path.exists("/Users/shir/PycharmProjects") else False
 if debug:
     output_dir = "output_spell"
     out_file = ""
+    nodes_dir = "data/all_nodes.pkl"
 else:
     output_dir = "/home/nlp/shirash1/Coreference-Graph/output_spell"
     out_file = "__1"
+    nodes_dir = "/home/nlp/shirash1/Coreference-Graph/fcoref/data/all_nodes.pkl"
 
 
 with open(os.path.join(output_dir, "dict_spell" + out_file + ".pkl"), "rb") as f_spell:
     dict_spell = pickle.load(f_spell)
-    print(len(dict_spell))
+    print("len spell dict:", len(dict_spell))
 
 c_in = 0
 c_out = 0
-with open("data/all_nodes.pkl", 'rb') as f:
+with open(nodes_dir, 'rb') as f:
     all_nodes = list(pickle.load(f))
     for node in all_nodes:
         if node in dict_spell:
