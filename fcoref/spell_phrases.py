b74264fb5026b1d5be25e0ab4b9ff93ad13b4b74
ShirApp <shirashury@gmail.com>
2024-06-15 22:27:50 +0300

    spell count

---
+++ fcoref/spell_phrases.py
@@ -0,0 +1,23 @@
+import os
+import pickle
+from collections import defaultdict
+
+output_dir = "output"
+out_file = ""
+
+with open(os.path.join(output_dir, "chains" + out_file + ".txt"), "r") as f_chains:
+    dict_spell = defaultdict(dict)
+    for line in f_chains.readlines():
+        chain = line[:-1].split('\t')
+        lower_chain = [ph.lower() for ph in chain]
+        for ind in range(len(chain)):
+            if chain[ind] not in dict_spell[lower_chain[ind]]:
+                dict_spell[lower_chain[ind]][chain[ind]] = 0
+            dict_spell[lower_chain[ind]][chain[ind]] += 1
+
+print(dict_spell)
+output_dir = "./output_spell"
+os.makedirs(output_dir, exist_ok=True)
+
+with open(os.path.join(output_dir, "dict_spell" + out_file + ".pkl"), "wb") as f_spell:
+    pickle.dump(dict_spell, f_spell)
